c6639d092e3297dd691ca8cb50ec192e5b56da2e
mrtauceti <prooks@gmail.com>
2015-10-14 21:28:45 +0200

    Lesson04.00
    Signed-off-by: mrtauceti <prooks@gmail.com>

--- Snake/SnakeGame.cs
+++ Snake/SnakeGame.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -12,6 +13,55 @@ namespace Snake
         {
             var p = new Point{Img = '#', X = 2, Y = 5};
             p.Draw();
+
+            int rowV = 0;
+            int colV = 0;
+            int row = 10;
+            int col = 10;
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey();
+
+                    if (key.Key == ConsoleKey.LeftArrow)
+                    {
+                        rowV = 0;
+                        colV = -1;
+                    }
+
+                    if (key.Key == ConsoleKey.RightArrow)
+                    {
+                        rowV = 0;
+                        colV = 1;
+                    }
+
+                    if (key.Key == ConsoleKey.UpArrow)
+                    {
+                        rowV = -1;
+                        colV = 0;
+                    }
+
+                    if (key.Key == ConsoleKey.DownArrow)
+                    {
+                        rowV = 1;
+                        colV = 0;
+                    }
+                }
+
+                row += rowV;
+                col += colV;
+                
+                Console.Clear();
+                Console.SetCursorPosition(col,row);
+                Console.Write('o');
+                
+                Thread.Sleep(500);
+            }
+
+
         }
     }
 }
